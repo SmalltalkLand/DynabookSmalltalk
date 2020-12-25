9ec60f81a443d57de892812a695bd7b382dfefda
Graham Kelly <grahamkelly@localhost.localdomain>
2020-12-25 16:39:24 -0500

    stuff

--- .ContinuousIntegrationScripts/installCuis.sh
+++ .ContinuousIntegrationScripts/installCuis.sh
@@ -1,8 +1,10 @@
 pip install mega.py
 #python -c "from mega import Mega;mega = Mega();mega.login('smalltalkland0@gmail.com','$MEGA');mega.download(mega.find('Cuis5.0-4426.image'));"
+echo "GZipping Squeak"
 cat release.image.gz | gzip -d > Cuis5.0-4426.image
 #sqcogspur64linuxht/bin/squeak -vm-display-null Cuis5.0-4426.image -d "Smalltalk at: #Auto put: #x.BaseLine fileOutSystem."  -d "Smalltalk at: #Auto put: #x.Smalltalk snapshot: true andQuit: true clearAllClassState: true."
+echo "Starting Squeak"
 sqcogspur64linuxht/bin/squeak -vm-display-null Cuis5.0-4426.image -d "Smalltalk at: #Auto put: #x.BaseLine fileOutSystem. Smalltalk snapshot: true andQuit: true embedded: false clearAllClassState: true."
 echo "Cuis install done"
 cat Cuis5.0-4426.image | gzip > release.image.gz
-
+echo "bundle done"
