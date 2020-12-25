741eb4aa8d2b07cb6ccd5abcc649d27a85f73012
Graham Kelly <grahamkelly@localhost.localdomain>
2020-12-25 09:19:36 -0500

    stuff

---
+++ .ContinuousIntegrationScripts/installCuis.sh
@@ -0,0 +1,2 @@
+git clone https://github.com/Cuis-Smalltalk/Cuis-Smalltalk-Dev.git
+./sqcogspur64linuxht/bin/squeak -vm-display-null Cuis-Smalltalk-Dev/Cuis*.image -s index.bl.st -d "Smalltalk snapshot: true andQuit: true clearAllClassState: false."
