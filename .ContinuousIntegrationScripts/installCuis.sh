eb366ecb8144a170587f3411cacc8cfb6e562920
Graham Kelly <grahamkelly@localhost.localdomain>
2020-12-25 18:55:41 -0500

    stuff

--- .ContinuousIntegrationScripts/installCuis.sh
+++ .ContinuousIntegrationScripts/installCuis.sh
@@ -4,7 +4,7 @@ echo "GZipping Squeak"
 cat release.image.gz | gzip -d > Cuis5.0-4426.image
 #sqcogspur64linuxht/bin/squeak -vm-display-null Cuis5.0-4426.image -d "Smalltalk at: #Auto put: #x.BaseLine fileOutSystem."  -d "Smalltalk at: #Auto put: #x.Smalltalk snapshot: true andQuit: true clearAllClassState: true."
 echo "Starting Squeak"
-sqcogspur64linuxht/bin/squeak -vm-display-null Cuis5.0-4426.image -d "Smalltalk at: #Auto put: #x.[(Delay forSeconds: 290) wait. StdIOWriteStream stdout newLine; nextPutAll: 'Failed build, continuing'.Smalltalk quitPrimitive: 1] fork. \"BaseLine fileOutSystem.\" [[[Smalltalk at: #Auto ifAbsent: nil] whileNotNil: [Processor yield]. Smalltalk quitPrimitive] fork. Smalltalk snapshot: true andQuit: false embedded: false clearAllClassState: true. ] on: Error do: [:ex|StdIOWriteStream stdout newLine; nextPutAll: ex printString]."
+sqcogspur64linuxht/bin/squeak -vm-display-null Cuis5.0-4426.image -d "Smalltalk at: #Auto put: #x.[(Delay forSeconds: 290) wait. StdIOWriteStream stdout newLine; nextPutAll: 'Failed build, continuing'.Smalltalk quitPrimitive: 1] fork. \"BaseLine fileOutSystem.\" [UISupervisor whenUIinSafeState: [Smalltalk reset;snapshot: true andQuit: true]] on: Error do: [:ex|StdIOWriteStream stdout newLine; nextPutAll: ex printString]."
 sqcogspur64linuxht/bin/squeak -vm-display-null Cuis5.0-4426.image -d "Smalltalk at: #Auto put: #x.[(Delay forSeconds: 290) wait. StdIOWriteStream stdout newLine; nextPutAll: 'Failed build, continuing'.Smalltalk quitPrimitive: 1] fork. BaseLine fileOutSystem.\" Smalltalk snapshot: true andQuit: true embedded: false clearAllClassState: true.\". Smalltalk quitPrimitive"
 echo "Cuis install done"
 cat Cuis5.0-4426.image | gzip > release.image.gz
