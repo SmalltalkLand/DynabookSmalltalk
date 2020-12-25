1d0db3313bd3eca3ea3e841e6e0308a776028654
Graham Kelly <grahamkelly@localhost.localdomain>
2020-12-25 16:42:26 -0500

    stuff

--- .ContinuousIntegrationScripts/installCuis.sh
+++ .ContinuousIntegrationScripts/installCuis.sh
@@ -1,4 +1,4 @@
-pip install mega.py
+
 #python -c "from mega import Mega;mega = Mega();mega.login('smalltalkland0@gmail.com','$MEGA');mega.download(mega.find('Cuis5.0-4426.image'));"
 echo "GZipping Squeak"
 cat release.image.gz | gzip -d > Cuis5.0-4426.image
